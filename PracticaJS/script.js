d04e5790e995bc07659e862f6450ccf4284c1396
SanchezPepe <sanchezpepe97@gmail.com>
2016-10-18 21:14:36 -0500

    Clase 18/10/2016

--- PracticaJS/script.js
+++ PracticaJS/script.js
@@ -29,7 +29,7 @@ document.querySelector('#boton').onclick=function(){
 }
 */
 
-function fibonacci(repeticiones){
+function fibonacci(){
   var vueltas = document.querySelector('#vueltasFib').value;
   prim = 0;
   seg = 1;
@@ -43,4 +43,31 @@ function fibonacci(repeticiones){
     seg = tercero;
     n++;
   }
+}
+
+function palindromo(pal){
+  var oracion = document.querySelector('#oracion').value;
+  resp = "No es palindromo";
+  var termine = false;
+  while(!termine){
+    i = 0;
+    j = oracion.length-1;
+    if(oracion[i] == " "){
+      i++;
+    }
+    if (oracion[j] == " ") {
+      j--;
+    }
+    if(oracion[j] == oracion[i]){
+      if(i == j || j < i){
+        resp = "Si es palindromo";
+        termine = true;
+      }
+    }
+    else
+      termine = true;
+    i++;
+    j--;
+  }
+  console.log(resp);
 }
